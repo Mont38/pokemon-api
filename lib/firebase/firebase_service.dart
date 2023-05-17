0c6813069e60925c0f4d6ed0e179e69bf1e4a471
Mont38 <montsetapia848@gmail.com>
2023-05-16 22:17:52 -0600

    Guardar cambios locales en firebase_service.dart

--- lib/firebase/firebase_service.dart
+++ lib/firebase/firebase_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 FirebaseFirestore db = FirebaseFirestore.instance;
 
@@ -18,4 +19,24 @@ Future<void> addUsers(
     String email, String password, String image, String name) async {
   await db.collection("users").add(
       {"email": email, "password": password, "image": image, "name": name});
+}
+
+void verificarCampoContrasena(BuildContext context) async {
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  DocumentSnapshot snapshot = await FirebaseFirestore.instance
+      .collection('users')
+      .doc(currentUser?.uid)
+      .get();
+
+  if (snapshot.exists &&
+      snapshot.data() != null &&
+      (snapshot.data() as Map<String, dynamic>).containsKey('password')) {
+    print('El campo "contrasena" existe para el usuario actual.');
+
+    Navigator.pushNamed(context, '/Home');
+  } else {
+    print('El campo "contrasena" no existe para el usuario actual.');
+    Navigator.pushNamed(context, '/Password_Google');
+  }
 }
